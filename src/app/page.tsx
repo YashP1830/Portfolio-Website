145ebccba12ece23a85391844803405a39e7d829
Yash1386 <yashpratap291@gmail.com>
2026-06-24 11:44:48 +0530

    refactor: remove unused component imports from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import Header from "@/components/Header";
 import ScrollProgress from "@/components/ScrollProgress";
 import Hero from "@/components/Hero";
 import Work from "@/components/Work";
-import Timeline from "@/components/Timeline";
-import Philosophy from "@/components/Philosophy";
-import Writing from "@/components/Writing";
-import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
